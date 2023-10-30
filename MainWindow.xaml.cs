97e3e45e6e8642b2851050d1ba60c59549c0bc99
hyktv <hafsa.yktv@gmail.com>
2023-10-30 01:07:52 -0700

    messagebox is left

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -18,11 +18,139 @@ namespace GUI_HafsaM
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
+    //HAFSA MOHAMED OCT. 30 - GUI_HafsaM
+
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
         }
-    }
-}
+
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            //create string variables
+            string num1 = txtNum1.Text;
+            string num2 = txtNum2.Text;
+
+            //parse string variables into numbers
+            int numba1 = int.Parse(num1);
+            int numba2 = int.Parse(num2);
+
+            //create sign change
+            //(the add or subtract label)
+            string sign = "+";
+            lblAddOrSubtract.Content = sign;
+
+            //add the 2 numbers together
+            //to get the result
+            int result = numba1 + numba2;
+
+            //idek why i decided to change
+            //the result back to string and put
+            //it into the result txtbox BUT IT WORKED
+            string w = $"{result}";
+            txtResult1.Text = w;
+
+        }//addition button
+
+
+
+        private void btnSubtract_Click(object sender, RoutedEventArgs e)
+        {
+            //create string variables
+            string num1 = txtNum1.Text;
+            string num2 = txtNum2.Text;
+
+            //parse string variables into numbers
+            int numba1 = int.Parse(num1);
+            int numba2 = int.Parse(num2);
+
+            //create sign change
+            //(the add or subtract label)
+            string sign = "-";
+            lblAddOrSubtract.Content = sign;
+
+            //create equation and display result
+            int result1 = numba1 - numba2;
+            string x = $"{result1}";
+            txtResult1.Text = x;
+
+        }//subtraction button
+
+
+
+        private void btnMultiply_Click(object sender, RoutedEventArgs e)
+        {
+            //create string variables
+            string num3 = txtNum3.Text;
+            string num4 = txtNum4.Text;
+
+            //parse string variables into numbers
+            int numba3 = int.Parse(num3);
+            int numba4 = int.Parse(num4);
+
+            //create sign change
+            //(the add or subtract label)
+            string sign = "*";
+            lblMultipyOrDivide.Content = sign;
+
+            //create equation and display result
+            int result2 = numba3 * numba4;
+            string y = $"{result2}";
+            txtResult2.Text = y;
+
+        }//multiplication button
+
+
+
+        private void btnDivide_Click(object sender, RoutedEventArgs e)
+        {
+            //create string variables
+            string num3 = txtNum3.Text;
+            string num4 = txtNum4.Text;
+
+            //parse string variables into numbers
+            int numba3 = int.Parse(num3);
+            int numba4 = int.Parse(num4);
+
+            //create sign change
+            //(the add or subtract label)
+            string sign = "/";
+            lblMultipyOrDivide.Content = sign;
+
+            //create equation and display result
+            int result3 = numba3 / numba4;
+            string z = $"{result3}";
+            txtResult2.Text = z;
+
+        }//division button
+
+
+
+        private void btnAverage_Click(object sender, RoutedEventArgs e)
+        {
+            //lets find out how to display the average
+            //(result 1 + result 2) / 2 = AVERAGE
+            string a = txtResult1.Text;
+            string b = txtResult2.Text;
+
+            //idk if we're supposed to parse but lets try
+            int result1 = int.Parse(a);
+            int result2 = int.Parse(b);
+
+            //create the equation and display the result
+            int equation = (result1 + result2) / 2;
+            string c = $"{equation}";
+            txtAverage.Text = c;
+
+        }//average button
+
+
+
+    }//class
+
+
+
+}//namespace
